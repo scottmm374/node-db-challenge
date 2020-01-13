0f519229c67e1585d891a7350bb9fb155c7dd942
Michelle Scott <scottmm374@gmail.com>
2020-01-12 21:06:21 -0500

    Resourced model and router with get and post complete

---
+++ API/resources-router.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const resource = require("./resources-model");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const data = await resource.getResources();
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ Error: "Something went wrong" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const newResource = req.body;
+  try {
+    const data = await resource.addResource(newResource);
+    res.status(201).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ Error: "Could not post project" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const projectsRouter = require("./API/projects-router.js");
+const resourceRouter = require("./API/resources-router.js");
 
 const server = express();
 server.use(express.json());
 server.use("/api/projects", projectsRouter);
+server.use("/api/resources", resourceRouter);
 
 server.get("/", (req, res) => {
   res.json({ Message: "Welcome to your projects" });
